38426fa20a5f902233be422a67b31eaa5291ecd3
Sohom Roy <rsohom426@gmail.com>
2026-08-08 23:41:34 +0530

    2nd & 3rd pattern program added

---
+++ inverted_half_start_pyramid_pattern.c
@@ -0,0 +1,21 @@
+/* C Program to print inverted half star pyramid pattern */
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
