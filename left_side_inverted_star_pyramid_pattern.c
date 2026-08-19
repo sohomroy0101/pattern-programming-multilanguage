735b644f5f352cce2ca587700647b7529829231e
Sohom Roy <rsohom426@gmail.com>
2026-08-19 23:10:26 +0530

    9th pattern program code added

---
+++ left_side_inverted_star_pyramid_pattern.c
@@ -0,0 +1,26 @@
+//C Program to print left side inverted star pattern
+#include <stdio.h>
+int main() {
+    int n, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for (j = i; j <= n; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=n; i>=1; i--){
+        for(j=i; j<=n; j++){
+            printf(" ");
+        }
+        for(j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
